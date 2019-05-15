e04d3096aa7b6ccafdc5103def60364e248a9ebf
Mark <marklensink@gmail.com>
2019-05-15 13:29:49 +0000

    Update on files

--- i2ctest/main.c
+++ i2ctest/main.c
@@ -1,11 +1,46 @@
-#include <stdio.h>
+#include<stdio.h>
+#include <string.h>
 #include "include/sensehat.h"
-
-int main(char* argv, int argc)
+#include <stdint.h>
+#define EVER ;;
+typedef struct color
 {
-	int file_i2c;
-	int length;
-	unsigned char buffer[60] = {0};
+	uint8_t R;
+	uint8_t G;
+	uint8_t B;	
+}color_t;
+
+// The LED Array is handled by a dedicated microcontroller
+// It must be updated in a single shot by writing
+// 192 bytes starting at register 0
+// The memory is laid out in each row like this:
+// RRRRRRRRGGGGGGGGBBBBBBBB
+// Each byte can have 64 unique levels (0-63)
+// 
+static unsigned char LEDArray[192] = {0};
+static int file_i2c;
+int shSetPixel(int x, int y, color_t color, int bUpdate)
+{
+	int i;
+	if (x >= 0 && x < 8 && y >= 0 && y < 8)
+	{
+		i = (y*24)+x; // offset into array
+		LEDArray[i] = color.R & 0x3e; // Red
+		LEDArray[i+8] = color.G & 0x3f; // Green
+		LEDArray[i+16] = color.B & 0x3e; // Blue
+		if (bUpdate)
+		{
+		//	writeslave(1,buffer,file_i2c);
+			writeslave(192, LEDArray, file_i2c); // have to send the whole array at once
+		}
+		return 1;
+	}
+	return 0;
+} /* shSetPixel() */
+
+
+int main ()
+{	
 	//----- OPEN THE I2C BUS -----
 	char *filename = (char*)"/dev/i2c-1";
 	file_i2c = openbus(filename);
@@ -13,28 +48,18 @@ int main(char* argv, int argc)
 	{
 		return -1;
 	}
-/*
-	int adrr = 0x5f;
-	openslave(adrr, file_i2c);
-	unsigned char out[2]= {0};
-	unsigned char in[1] = {0x28};
-	writeslave(1,in,file_i2c);
-	readslave(1,out,file_i2c);
-	printf("Low register (28h): %x\n",out[0]);
-	in[0] = 0x29;
-	writeslave(1,in,file_i2c);
-	readslave(1,out,file_i2c);
-	printf("High register(29h): %x\n",out[0]);
-*/
-
+	color_t setcolor = {255,0,0};	
+	color_t empty = {0,0,0};
 	int addr = 0x46;
 	openslave(addr,file_i2c);
-	unsigned char in[] = {0,0,0,0};
-	writeslave(4,in,file_i2c); 
-
+	shSetPixel(1,0,empty,1);
+	shSetPixel(1,1,empty,1);
+	shSetPixel(1,2,empty,1);
+	shSetPixel(1,3,empty,1);
+	shSetPixel(1,4,empty,1);
+	shSetPixel(1,5,empty,1);
+	shSetPixel(1,6,empty,1);
+	shSetPixel(1,7,empty,1);
+		
 }
-/*
-		self.i2cbus.write_byte_data(self.ctrl_address, x*24+y, color[0])
-		self.i2cbus.write_byte_data(self.ctrl_address, x*24+y+8, color[1])
-		self.i2cbus.write_byte_data(self.ctrl_address, x*24+y+16, color[2])
-*/
+
--- i2ctest/sense_hat_unchained-master/demo/main.c
+++ i2ctest/sense_hat_unchained-master/demo/main.c
@@ -12,21 +12,22 @@
 #include <stdint.h>
 #include <string.h>
 #include <sensehat.h>
+
 //
 // Update a simple pattern on the LED matrix
 //
+static int red;
 void UpdatePattern(void)
 {
-static int iOffset = 0;
-int i, j, x, y;
-uint16_t color;
-
-	for (i=0; i<64; i++) // do 64 pixels (whole matrix)
+	static int iOffset = 0;
+	int i, j, x, y;
+	uint16_t color;
+	for (i=0; i<8; i++) // do 64 pixels (whole matrix)
 	{
-		j = i + iOffset;
-		color = ((j & 8)>>3) * (j & 31); // blue
-		color |= (((j & 16)>>4) * (j & 63)<<5); // green
-		color |= (((j & 32)>>5) * (j & 31)<<11); // red
+		i = i + iOffset;
+		color = ((red & 8)>>3) * (red & 31); // blue
+		color |= (((0 & 16)>>4) * (0 & 63)<<5); // green
+		color |= (((0 & 32)>>5) * (0 & 31)<<11); // red
 		x = i & 7;
 		y = (i >> 3);
 		shSetPixel(x, y, color, 0);
@@ -40,7 +41,7 @@ int main(int argc, char *argv[])
 int i;
 unsigned char ucKeys;
 
-	if (shInit(0) == 0) // Open I2C	
+	if (shInit(1) == 0) // Open I2C	
 	{
 		printf("Unable to open sense hat; is it connected?\n");
 		return -1;
@@ -48,26 +49,10 @@ unsigned char ucKeys;
 	ucKeys = 0;
 	while (ucKeys == 0) // run until joystick pressed
 	{
-	int t, h, p, x, y, z;
-		ucKeys = shReadJoystick();
-		printf("keys = %02x\n", ucKeys);
-		if (shGetTempHumid(&t, &h))
-		{
-			printf("T=%d, H=%d\n", t, h);
-		}
-		if (shGetPressure(&p, &t))
-		{
-			printf("P=%d, T=%d\n", p, t);
-		}
-		if (shGetMagneto(&x, &y, &z))
-		{
-			printf("accX=%d, accY=%d, accZ=%d\n", x, y, z);
-		}
-		for (i=0; i<5; i++) // update pixels 5x as fast as sensor readings
-		{
-			UpdatePattern();
-			usleep(100000);
-		}
+		red = 45;
+		UpdatePattern();
+		red = 0;
+		UpdatePattern();
 	} // while waiting for joystick press
 	shShutdown();
 	return 0;
