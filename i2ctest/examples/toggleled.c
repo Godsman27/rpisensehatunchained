33581942ee6912a207545d2e273ae754baab84b7
Mark <marklensink@gmail.com>
2019-05-21 11:51:52 +0000

    toggleled push

--- i2ctest/examples/toggleled.c
+++ i2ctest/examples/toggleled.c
@@ -32,7 +32,6 @@ B,Y,B,Y,B,Y,B,Y//
 
 uint16_t pat3[64] = 
 {//
-Y,Y,Y,Y,Y,Y,Y,Y,//
 Y,Y,O,O,O,O,Y,Y,//
 Y,O,Y,O,O,Y,O,Y,//
 Y,O,O,Y,Y,O,O,Y,//
@@ -60,20 +59,36 @@ int main ()
 	InitDisplay(1);
 	int r = InitHTS(1);
 	double temp =0, hum = 0;
-	printf("HTS returned: %i\n",r);
+	printf("HTS init returned: %i\n",r);
+	r = InitLPSH(1);
+	printf("LPSH init returned: %i\n",r);
+	double pressure=0,ptemp =0;
+	int mx=0,my=0,mz=0,gx=0,gy=0,gz=0,ax=0,ay=0,az=0;
+	r = InitLSM(1);
+	printf("LSM init returned: %i\n",r);
 	for(EVER)
 	{
-		sleep(1);
+//		sleep(1);
 		SetPattern(pat2,63);	
-		sleep(1);
-		SetPattern(pat1,63);
-		//sleep(2);
+
+		//sleep(1);
 		//SetPattern(pat3,63);
-		//sleep(2);
+		//sleep(1);
 		//SetPattern(pat4,63);
 		ReadTemp(&temp);
 		ReadHumidity(&hum);
-		printf("temperature is: %f, Huminity = %f\n",temp,hum);
-	}
+		ReadPressure(&pressure);
+		ReadPtemp(&ptemp);
+		ReadGyro(&gx,&gy,&gz);
+		ReadMagnet(&mx,&my,&mz);
+		ReadAccel(&ax,&ay,&az);
+		printf("Temperature is: %6.6f, Huminity = %6.6f\n",temp,hum);
+		printf("Pressure is: %6.6f, Pressure temperature = %6.6f\n",pressure,ptemp);
+		printf("Gyros_X = %6.1i, Gyros_Y = %6.1i, Gyros_Z = %6.1i\n",gx,gy,gz);
+		printf("Accel_X = %6.1i, Accel_Y = %6.1i, Accel_Z = %6.1i\n",ax,ay,az);
+		printf("Magne_X = %6.1i, Magne_Y = %6.1i, Magne_Z = %6.1i\n",mx,my,mz);
+		sleep(1);
+		SetPattern(pat1,63);
+	}		
 }
 
