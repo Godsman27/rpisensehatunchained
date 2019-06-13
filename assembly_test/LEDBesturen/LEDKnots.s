99bbc604cb5bca1fb3aeaeeed079e3059018e4c9
Woefie <larsh.hanssen@gmail.com>
2019-06-13 15:33:10 +0200

    changes to assembly

--- assembly_test/LEDBesturen/LEDKnots.s
+++ assembly_test/LEDBesturen/LEDKnots.s
@@ -10,23 +10,114 @@ I2C_SLAVE:	.word 	0x0703
 something:	.word	0xf2
 fd:			.word	0
 return:		.word 	0
+return2:	.word	0
 x:			.word	0
 y:			.word	0
 COLOR:		.word	0xF800
-array:		.skip	4
+CLEAR:		.word	0x0000
+array:		.skip	3
 LEDArray: 	.skip 	193
 
 
 .text
+
+
+reset:
+
+	ldr r2, =x
+	ldr	r2, [r2]
+	ldr r3, =y
+	ldr	r3, [r3]
+
+	ldr r6, =LEDArray
+				@ i = (y * 24) + x
+	mov r8, #24
+	mla	r1, r8, r1, r0
+	add r1, r6, r1		
+	add r1, r1, #1
+	
+	ldr r2, =CLEAR
+	ldr r2, [r2]
+
+
+	@ldr r1, [r8]
+	ldr	r0, =message2
+	bl printf
+	
+
+
+	mov	r8, r2, LSR #10
+	and r8, #0x3e
+	str r8, [r1]
+	
+		mov	r8, r2, LSR #5
+	and r8, #0x3f
+	str	r8, [r1, #+8]
+	
+	mov	r8, r2, LSL #1
+	and r8, #0x3e
+	str r8, [r1, #+16]
+	
+	push	{lr}
+	bl write
+	pop	{lr}
+	
+	bx	lr
+
+
+write:
+	ldr r0, =fd
+	ldr r0,	[r0]
+	ldr r1, =LEDArray	@ array with led information
+	mov r2, #193		@ size of the array
+	mov r7, #4			@ system call write
+	swi 0
+
+	
+	bx	lr
+
+	
+ledLoop:
+			@ if
+	ldr r6, =LEDArray
+	
+			@ i = (y * 24) + x
+	mov r8, #24
+	mla	r1, r8, r1, r0
+	add r1, r6, r1		
+	add r1, r1, #1
+
+	ldr	r2, =COLOR
+	ldr r2, [r2]
+
+	
+	mov	r8, r2, LSR #10
+	and r8, #0x3e
+	str r8, [r1]
+
+	mov	r8, r2, LSR #5
+	and r8, #0x3f
+	str	r8, [r1, #+8]
+	
+	mov	r8, r2, LSL #1
+	and r8, #0x3e
+	str r8, [r1, #+16]
+
+	
+
+	push	{lr}
+	bl write
+	pop	{lr}
+
+
+	bx lr
+	
+
+
 .global main
-.type 	main, %function
 
 main:
-	ldr r7, =return
-	str lr, [r7]
-
-
-
+	push	{lr}
 	ldr r0, =file 	
 	ldr r1, =O_RDWR
 	ldr	r1, [r1]
@@ -36,12 +127,8 @@ main:
 
 	ldr r1, =fd
 	str r0, [r1]
-	
-	
-	@mov r1, r0
-	@ldr r0, =message2
-	@bl	printf
 
+	
 	ldr	r0, =fd
 	ldr r0, [r0]
 	ldr r1, =I2C_SLAVE	@ r1 = &I2C_SLAVE
@@ -52,11 +139,18 @@ main:
 	
 	mov r7, #0x36		@ system call ioctl
 	swi	0
-
-
 leds:
-
 	
+
+	ldr r1, =array	
+	ldr r1, [r1]
+	ldr r2, =x
+	ldr	r2, [r2]
+	ldr r3, =y
+	ldr	r3, [r3]
+	
+	ldr	r0, =message1
+	bl printf
 
 @ if x < 0 && x >=8	
 	ldr r0, =x
@@ -74,59 +168,20 @@ other:					@ if y< 0 && y >= 8
 	bhs end
 	
 	
-			@ if
-	ldr r6, =LEDArray
-	
-
-	
-
-	
-			@ i = (y * 24) + x
-	mov r8, #24
-	mla	r1, r8, r1, r0
-	add r1, r6, r1		
-	add r1, r1, #1
-
-	ldr	r2, =COLOR
-	ldr r2, [r2]
-	
-	
-
-
-	mov	r8, r2, LSR #10
-	and r8, #0x3e
-	str r8, [r1]
-
-	mov	r8, r2, LSR #5
-	and r8, #0x3f
-	str	r8, [r1, #+8]
-	
-	mov	r8, r2, LSL #1
-	and r8, #0x3e
-	str r8, [r1, #+16]
-
-		
 @	mov	r8, #0 
 @	str r8, [r6]	
 	
 
+	bl	ledLoop
 
-
-
-	ldr r0, =fd
-	ldr r0,	[r0]
-	ldr r1, =LEDArray	@ array with led information
-	mov r2, #193		@ size of the array
-	mov r7, #4			@ system call write
-	swi 0
-	
-	
 	
 
 Loop:
 	
+
+
 	mov r4, #0xFF
-	lsl r4, #20
+	lsl r4, #19
 1:
 	subs	r4, r4, #1
 	bne 1b
@@ -155,24 +210,36 @@ Loop:
 	ldr	r3, =y
 	ldr r3, [r3]
 
+press:
 	cmp r1, #0x8
 	beq	end
 
 yup:	
 	cmp r1, #0x1
 	bne xup
+	
+	ldr r5, =COLOR
+	mov r7, #0x0000
+	str	r7, [r5]
+	bl ledLoop
+	ldr r5, =COLOR
+	mov r7, #0xf800
+	str	r7, [r5]
+	
 	ldr r4, =y
 	add r3, r3, #1
 	str r3, [r4]
 	beq	leds
-
+	
 xup:
 	cmp r1, #0x2
 	bne ydown
+	
 	ldr r4, =x
 	add r2, r2, #1
 	str r2, [r4]
 	beq	leds
+	
 
 ydown:	
 	cmp r1, #0x4
@@ -181,6 +248,7 @@ ydown:
 	sub r3, r3, #1
 	str r3, [r4]
 	beq	leds
+			
 
 xdown:
 	cmp r1, #0x10
@@ -189,27 +257,20 @@ xdown:
 	sub r2, r2, #1
 	str r2, [r4]
 	beq	leds
+	
 
 endknots:
-	ldr r1, =array	
-	ldr r1, [r1]
-	ldr r2, =x
-	ldr	r2, [r2]
-	ldr r3, =y
-	ldr	r3, [r3]
-	
-	ldr	r0, =message1
-	bl printf
-	
+
+		
 	b	Loop	
 
 
 end:
+	
 	mov r7, #6
 	swi 0
-
-	ldr r7, =return
-	ldr lr, [r7]
+	
+	pop	{lr}
 	
 	bx	lr
 
